5d2787a94d730a0ae9e5b446feb0a78420e4ab5e
rconjoe <joe@rconjoe.com>
2022-02-27 12:31:15 -0800

    init web app

---
+++ web/src/App.vue
@@ -0,0 +1,18 @@
+<script setup>
+import PowerButton from './components/PowerButton.vue'
+</script>
+
+<template>
+  <nav class="p-6 bg-pink-900 flex justify-between m-6 rounded-lg text-white">
+    <span class="p-1">Kassandra.js</span>
+    <a href="" class="p-1 hover:text-blue-300">Sign In</a>
+    </nav>
+
+    <div class="flex justify-center">
+    <PowerButton />
+  </div>
+
+</template>
+
+<style>
+</style>
---
+++ web/src/components/PowerButton.vue
@@ -0,0 +1,9 @@
+<script setup>
+import { ref } from 'vue'
+</script>
+
+<template>
+  <div class="w-8/12 bg-red-800 p-6 rounded lg">
+    
+  </div>
+</template>
