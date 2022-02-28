3683b46429a853c1304020c4572066f9a3ab9b93
rconjoe <joe@rconjoe.com>
2022-02-28 12:05:37 -0800

    sign in modal login

--- web/src/App.vue
+++ web/src/App.vue
@@ -1,14 +1,25 @@
 <script setup>
+import { ref } from 'vue'
 import PowerButton from './components/PowerButton.vue'
+import SignIn from './components/SignIn.vue'
+
+const signInModal = ref(false)
+
 </script>
 
 <template>
+
+  <!-- Header Bar -->
   <nav class="p-6 bg-pink-900 flex justify-between m-6 rounded-lg text-white">
     <span class="p-1">Kassandra.js</span>
-    <a href="" class="p-1 hover:text-blue-300">Sign In</a>
-    </nav>
+    <p @click="signInModal = !signInModal" class="p-1 hover:text-blue-300">Sign In</p>
+  </nav>
 
-    <div class="flex justify-center">
+
+    <SignIn :signInModal="signInModal" />
+
+    <!-- Button Row -->
+  <div class="flex justify-center">
     <PowerButton />
   </div>
 
---
+++ web/src/components/SignIn.vue
@@ -0,0 +1,13 @@
+<script setup>
+const props = defineProps({
+  signInModal: Boolean
+})
+</script>
+
+
+<template>
+  <div v-if="signInModal" class="modal">
+    signinmodal
+  </div>
+</template>
+
