bdd760de6e4b149d4578ce109bf5bb35c5309aaa
rconjoe <joe@rconjoe.com>
2022-02-28 15:55:37 -0800

    sign in logic, emit receiver method for modal toggling

--- web/src/App.vue
+++ web/src/App.vue
@@ -1,9 +1,20 @@
 <script setup>
-import { ref } from 'vue'
+import { ref, reactive } from 'vue'
 import PowerButton from './components/PowerButton.vue'
 import SignIn from './components/SignIn.vue'
 
 const signInModal = ref(false)
+const user = reactive({
+  username: "",
+  twitch_username: "",
+  uid: "",
+  email: "",
+  flags: []
+})
+
+function toggleModal() { 
+  signInModal.value = !signInModal.value
+}
 
 </script>
 
@@ -12,11 +23,12 @@ const signInModal = ref(false)
   <!-- Header Bar -->
   <nav class="p-6 bg-pink-900 flex justify-between m-6 rounded-lg text-white">
     <span class="p-1">Kassandra.js</span>
-    <p @click="signInModal = !signInModal" class="p-1 hover:text-blue-300">Sign In</p>
+    <p v-if="user.username === ''" @click="signInModal = !signInModal" class="p-1 hover:text-blue-300">Sign In</p>
+    <p v-else class="p-1 hover:text-blue-300">Sign Out</p>
   </nav>
 
 
-    <SignIn :signInModal="signInModal" />
+    <SignIn :signInModal="signInModal" @toggleModal="toggleModal" />
 
     <!-- Button Row -->
   <div class="flex justify-center">
