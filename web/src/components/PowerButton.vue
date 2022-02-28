69549fcfdc04eb69ab43d0c68e0acef97fdd8001
rconjoe <joe@rconjoe.com>
2022-02-28 12:05:25 -0800

    power button classing no api yet

--- web/src/components/PowerButton.vue
+++ web/src/components/PowerButton.vue
@@ -1,9 +1,16 @@
 <script setup>
-import { ref } from 'vue'
+import { ref, computed } from 'vue'
+
+const on = ref(true)
+
+const powerClass = computed(() => on.value ? 'bg-green-700' : 'bg-red-700' )
+const buttonClass = 'py-10 px-16 rounded-lg text-white hover:shadow-xl'
+
+function togglePower() {
+  on.value = !on.value
+}
 </script>
 
 <template>
-  <div class="w-8/12 bg-red-800 p-6 rounded lg">
-    
-  </div>
+  <button :class="[ powerClass, buttonClass ]" @click="togglePower">{{ on ? 'On' : 'Off' }}</button>
 </template>
