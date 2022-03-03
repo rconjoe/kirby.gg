b78240bd981806031782d1bab627ec43ff663ff4
rconjoe <joe@rconjoe.com>
2022-03-02 17:18:10 -0800

    break top main public splash into Hero component

---
+++ web/src/components/Hero.vue
@@ -0,0 +1,23 @@
+<script setup>
+</script>
+
+<template>    
+  <div class="flex justify-between items-start bg-pink-300 mx-16 mt-12 rounded-lg border-lg shadow-xl text-purple-900 text-center">
+    <img src="../assets/kassandra.png" class="scale-75" />
+    <div class="py-12 px-6 bg-pink-200 rounded-xl mt-24 mr-24">
+      <span class="italic text-xl">Hey there!</span>
+      <br />
+      <br />
+        <p class="text-lg">
+        I'm <strong>Kassandra</strong>. 
+        <br />
+        Nice to meet you! I'm the official Twitch channel bot at
+        <a href="https://twitch.tv/kcckirby" class="text-pink-900 hover:text-blue-900">Gaming With Kirby.</a>
+        <br />
+        If you'd like, you can check out my
+        <button class="px-2 rounded-xl bg-pink-400">commands</button>,
+        and be sure to tune in to the next stream!
+        </p>
+      </div>
+    </div>
+</template>
