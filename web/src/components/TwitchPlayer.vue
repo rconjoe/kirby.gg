c50d33c16a9e9211daa831c0ad3dda91d95f2e32
rconjoe <joe@rconjoe.com>
2022-03-02 17:18:46 -0800

    heres a working vue twitch player i can embed in other components

---
+++ web/src/components/TwitchPlayer.vue
@@ -0,0 +1,19 @@
+<script setup>
+import { useScriptTag } from '@vueuse/core'
+
+useScriptTag(
+  'https://player.twitch.tv/js/embed/v1.js',
+  (el) => {
+    new window.Twitch.Player("twitch-embed", {
+      channel: 'kcckirby',
+      height: 400,
+      width: 680,
+      theme: "dark"
+    })
+  }
+)
+</script>
+
+<template>
+  <div id="twitch-embed"></div>
+</template>
