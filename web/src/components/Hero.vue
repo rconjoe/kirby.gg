f429a72c4c301ea09857f6abac0c5f9bc6777d72
rconjoe <joe@rconjoe.com>
2022-03-05 10:53:58 -0800

    move around hero stuff to stretch across row. add kirbyfishing.jpg

--- web/src/components/Hero.vue
+++ web/src/components/Hero.vue
@@ -3,14 +3,14 @@ import Schedule from './Schedule.vue'
 </script>
 
 <template>    
-    <div class="mr-8 bg-pink-300 border-2 shadow-xl border-x-pink-600 border-y-pink-800 rounded-xl p-12 flex flex-row">
-      <img src="../assets/kassandra.png" class="max-w-xs"/>
+    <div class="flex flex-col-auto items-center justify-evenly bg-pink-300 border-2 shadow-xl border-x-pink-600 border-y-pink-800 rounded-xl p-12">
+      <img src="../assets/kassandra.png" class="max-w-sm"/>
       <div class="ml-10 text-center">
-        <p class="py-4">Hi! <br />I'm <strong>Kassandra!</strong></p>
-        <p>I'm the official bot for <a class="text-blue-700 hover:text-pink-900" href="https://twitch.tv/KCCKirby">Gaming With Kirby</a>.</p>
-        <p class="pt-4">Here's what Kirby told me about the schedule...</p>
-        <p class="pb-4">All times are in EST!</p>
+        <p class="py-4 text-xl">Hi! <br />I'm <strong>Kassandra!</strong></p>
+        <p class="text-xl">I'm the official bot for <a class="text-blue-700 hover:text-pink-900" href="https://twitch.tv/KCCKirby">Gaming With Kirby</a>.</p>
+        <p class="pt-4 pb-2 text-xl">Here's what Kirby told me about the schedule...</p>
       <Schedule />
       </div>
+      <img src="../assets/kirbyfishing.jpg" class="max-w-md"/>
     </div>
 </template>
