42f37eb4b8b965e07eaee64ad00ac7730dad2213
rconjoe <joe@rconjoe.com>
2022-03-05 10:52:49 -0800

    a component where we can say things as kassandra

---
+++ web/src/components/SayAsBot.vue
@@ -0,0 +1,11 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="flex flex-row m-1 col-span-2 justify-center">
+    <input type="text" placeholder="Say something (as Kassandra)!" class="bg-gray-200 border-2 w-full p-4 rounded-lg row-span-2" />
+    <button @click="sayAsKassandra" class="px-2 ml-2 bg-pink-800 rounded-lg text-white hover:shadow-xl">Send</button>
+  </div>
+</template>
+
