2ed8bcf67b428fd9e30fce8c1003457c907020e5
rconjoe <joe@rconjoe.com>
2022-03-04 19:56:34 -0800

    padding/margin fixes, spans to anchors

--- web/src/components/NavBar.vue
+++ web/src/components/NavBar.vue
@@ -6,7 +6,7 @@
 </script>
 
 <template>
-  <nav class="bg-pink-800 m-6 rounded-lg shadow-xl text-xl text-pink-100">
+  <nav class="bg-pink-800 mx-6 my-8 rounded-lg shadow-xl text-xl text-pink-100">
     <!-- <span class="py-12">Kassandra.js</span> -->
     <div class="grid grid-cols-5 place-items-center pt-6">
       <div class="grid grid-rows-3 place-items-center">
@@ -14,7 +14,7 @@
           <img src="../assets/k9.jpg" @click="$emit('setWindow', 0)" class="h-24 w-28 transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-125 hover:duration-300" />
         </div>
         <div class="row-span-1 text-center">
-          <span class="hover:text-blue-300" @click="$emit('setWindow', 0)">Home</span>
+          <a class="hover:text-blue-300" @click="$emit('setWindow', 0)">Home</a>
         </div>
       </div>
       <div class="grid grid-rows-3 text-center">
@@ -22,7 +22,7 @@
           <img src="../assets/k2.jpg" @click="$emit('setWindow', 1)" class="h-24 w-28 transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-125 hover:duration-300" />
         </div>
         <div class="row-span-1">
-          <span class="hover:text-blue-300" @click="$emit('setWindow', 1)">Commands</span>
+          <a class="hover:text-blue-300" @click="$emit('setWindow', 1)">Commands</a>
         </div>
       </div>
       <div class="grid grid-rows-3 text-center">
@@ -30,7 +30,7 @@
           <img src="../assets/k3.jpg" @click="$emit('setWindow', 2)" class="h-24 w-28 transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-125 hover:duration-300" />
         </div>
         <div class="row-span-1">
-          <span class="hover:text-blue-300" @click="$emit('setWindow', 2)">Mod</span>
+          <a class="hover:text-blue-300" @click="$emit('setWindow', 2)">Mod</a>
         </div>
       </div>
       <div class="grid grid-rows-3 text-center">
@@ -38,7 +38,7 @@
           <img src="../assets/k7.jpg" @click="$emit('setWindow', 3)" class="h-24 w-28 transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-125 hover:duration-300" />
         </div>
         <div class="row-span-1">
-          <span class="hover:text-blue-300" @click="$emit('setWindow', 3)">???</span>
+          <a class="hover:text-blue-300" @click="$emit('setWindow', 3)">???</a>
         </div>
       </div>
       <div class="grid grid-rows-3 text-center">
@@ -46,8 +46,8 @@
           <img src="../assets/k5.jpg" @click="$emit('modal')" class="h-24 w-28 transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-125 hover:duration-300" />
         </div>
         <div class="row-span-1">
-          <p v-if="uid === ''" class="hover:text-blue-300">Sign In</p>
-          <p v-else class="p-1 hover:text-blue-300">Sign Out</p>
+          <a v-if="uid === ''" @click="$emit('modal')" class="hover:text-blue-300">Sign In</a>
+          <a v-else class="p-1 hover:text-blue-300" @click="$emit('modal')">Sign Out</a>
         </div>
       </div>
     </div>
