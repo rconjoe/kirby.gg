efbb71a7060c4d4bb083defb9d22e9aa261ad933
rconjoe <joe@rconjoe.com>
2022-03-05 10:56:04 -0800

    resize player and move chat to right

--- web/src/components/TwitchPlayer.vue
+++ web/src/components/TwitchPlayer.vue
@@ -1,13 +1,14 @@
 <script setup>
 import { useScriptTag } from '@vueuse/core'
+import Chat from './Chat.vue'
 
 useScriptTag(
   'https://player.twitch.tv/js/embed/v1.js',
   (el) => {
     new window.Twitch.Player("twitch-embed", {
       channel: 'kcckirby',
-      height: 380,
-      width: 680,
+      height: 440,
+      width: 740,
       theme: "dark"
     })
   }
@@ -15,7 +16,8 @@ useScriptTag(
 </script>
 
 <template>
-  <div class="ml-8 bg-pink-300 border-2 shadow-xl border-x-pink-600 border-y-pink-800 p-6 rounded-xl">
-    <div id="twitch-embed"></div>
+  <div class="flex flex-row gap-4 justify-center items-center bg-pink-300 border-2 shadow-xl border-x-pink-600 border-y-pink-800 p-4 rounded-xl">
+    <div id="twitch-embed" class="border-2 border-x-pink-600 border-y-pink-800"></div>
+    <Chat />
   </div>
 </template>
