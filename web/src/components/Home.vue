a40d397f78855eba4f6cace6510eeaf768be1616
rconjoe <joe@rconjoe.com>
2022-03-04 19:55:57 -0800

    new home layout with hero on left and twitch/chat in column on right

--- web/src/components/Home.vue
+++ web/src/components/Home.vue
@@ -1,18 +1,24 @@
 <script setup>
 import PowerButton from './PowerButton.vue'
 import TwitchPlayer from './TwitchPlayer.vue'
+import Hero from './Hero.vue'
+import Chat from './Chat.vue'
 </script>
 
+
 <template>
-  <div class="my-12 grid grid-cols-2 grid-rows-2 justify-items-center items-start">
-    <div class="bg-pink-300 border-2 shadow-xl border-x-pink-600 border-y-pink-800 rounded-xl p-12 flex flex-row">
-      <img src="../assets/kassandra.png" class="max-w-sm"/>
-      <div class="m-16">
-        <p>Hey there, I'm <strong>Kassandra!</strong></p>
-      </div>
+
+  <!-- Grid div -->
+  <div class="grid grid-flow-col-dense row-span-2 justify-center items-start">
+
+    <div>
+      <Hero />
     </div>
-    <div class="bg-pink-300 border-2 shadow-xl border-x-pink-600 border-y-pink-800 p-10 rounded-xl">
-      <TwitchPlayer />
-  </div>
+    <div>
+      <div class="mb-6">
+        <TwitchPlayer />
       </div>
+        <Chat />
+      </div>
+  </div>
 </template>
