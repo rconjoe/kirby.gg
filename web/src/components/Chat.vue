b0d5a25b6e978f12933cb4f226aa662aee9b1ce2
rconjoe <joe@rconjoe.com>
2022-03-05 10:51:34 -0800

    restyle

--- web/src/components/Chat.vue
+++ web/src/components/Chat.vue
@@ -2,7 +2,7 @@
 </script>
 
 <template>
-  <div class="ml-8 bg-pink-300 border-2 shadow-xl border-x-pink-600 border-y-pink-800 p-6 rounded-xl flex justify-center align-items-center">
-    <iframe src="https://www.twitch.tv/embed/kcckirby/chat?darkpopout&parent=localhost" height="600" width="600" class="border-2 border-x-pink-600 border-y-pink-800 " />
-  </div>
+  <!-- <div class="ml-8 bg-pink-300 border-2 shadow-xl border-x-pink-600 border-y-pink-800 p-6 rounded-xl flex justify-center align-items-center"> -->
+    <iframe src="https://www.twitch.tv/embed/kcckirby/chat?darkpopout&parent=localhost" height="443" width="450" class="border-2 border-x-pink-600 border-y-pink-800" />
+  <!-- </div> -->
 </template>
