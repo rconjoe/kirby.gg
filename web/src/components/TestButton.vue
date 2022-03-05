ddbd4b12cec9b7663e25944230c620c17ad49729
rconjoe <joe@rconjoe.com>
2022-03-05 10:55:45 -0800

    init test button

---
+++ web/src/components/TestButton.vue
@@ -0,0 +1,6 @@
+<script setup>
+</script>
+
+<template>
+  <button class="py-8 px-14 m-2 rounded-lg border-2 bg-blue-800 border-gray-700 text-white transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-125 hover:duration-300" @click="console.log('test')">Test</button>
+</template>
