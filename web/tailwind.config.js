4c2378af64db73f7ae6a7e40a0bda531c80d0c44
rconjoe <joe@rconjoe.com>
2022-03-05 10:53:04 -0800

    add font

--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -1,10 +1,15 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
 module.exports = {
   content: [
     "./index.html",
     "./src/**/*.{vue,js,ts,jsx,tsx}"
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'spartan': ['"League Spartan"', ...defaultTheme.fontFamily.sans]
+      }
+    },
   },
   plugins: [],
 }
