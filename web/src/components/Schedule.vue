8c7d4169158ca56e93e141ae7cfa4488b24da473
rconjoe <joe@rconjoe.com>
2022-03-05 10:55:04 -0800

    replace strong tags with tailwind text-bold

--- web/src/components/Schedule.vue
+++ web/src/components/Schedule.vue
@@ -25,8 +25,8 @@ onMounted(async () => {
 </script>
 
 <template>
-  <div v-for="stream in schedule" :key="id" class="bg-pink-200 text-pink-900 p-2 my-4 justify-items-center items-center rounded-lg shadow-lg">
-    <p><strong>{{ stream.title }}</strong></p>
-    <p>{{ dayjs(stream.start_time).tz('America/New_York').format('ddd M/D h:mma') }}</p>
+  <div v-for="stream in schedule" :key="id" class="bg-pink-200 text-pink-900 p-3 my-4 justify-items-center items-center rounded-lg shadow-lg">
+    <p class="font-bold">{{ stream.title }}</p>
+    <p>{{ dayjs(stream.start_time).tz('America/New_York').format('ddd M/D h:mma') }} EST</p>
     </div>
 </template>
