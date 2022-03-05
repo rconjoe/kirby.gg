0a7af75d1736983ed16ac57e6f9f3c8a91eb9e64
rconjoe <joe@rconjoe.com>
2022-03-05 10:54:38 -0800

    shrink the nav bar down a little

--- web/src/components/NavBar.vue
+++ web/src/components/NavBar.vue
@@ -8,10 +8,10 @@
 <template>
   <nav class="bg-pink-800 mx-6 my-8 rounded-lg shadow-xl text-xl text-pink-100">
     <!-- <span class="py-12">Kassandra.js</span> -->
-    <div class="grid grid-cols-5 place-items-center pt-6">
+    <div class="grid grid-cols-6 place-items-center pt-6">
       <div class="grid grid-rows-3 place-items-center">
         <div class="row-span-2">
-          <img src="../assets/k9.jpg" @click="$emit('setWindow', 0)" class="h-24 w-28 transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-125 hover:duration-300" />
+          <img src="../assets/k9.jpg" @click="$emit('setWindow', 0)" class="h-20 w-24 transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-125 hover:duration-300" />
         </div>
         <div class="row-span-1 text-center">
           <a class="hover:text-blue-300" @click="$emit('setWindow', 0)">Home</a>
@@ -19,7 +19,7 @@
       </div>
       <div class="grid grid-rows-3 text-center">
         <div class="row-span-2">
-          <img src="../assets/k2.jpg" @click="$emit('setWindow', 1)" class="h-24 w-28 transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-125 hover:duration-300" />
+          <img src="../assets/k2.jpg" @click="$emit('setWindow', 1)" class="h-20 w-24 transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-125 hover:duration-300" />
         </div>
         <div class="row-span-1">
           <a class="hover:text-blue-300" @click="$emit('setWindow', 1)">Commands</a>
@@ -27,7 +27,7 @@
       </div>
       <div class="grid grid-rows-3 text-center">
         <div class="row-span-2">
-          <img src="../assets/k3.jpg" @click="$emit('setWindow', 2)" class="h-24 w-28 transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-125 hover:duration-300" />
+          <img src="../assets/k3.jpg" @click="$emit('setWindow', 2)" class="h-20 w-24 transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-125 hover:duration-300" />
         </div>
         <div class="row-span-1">
           <a class="hover:text-blue-300" @click="$emit('setWindow', 2)">Mod</a>
@@ -35,7 +35,7 @@
       </div>
       <div class="grid grid-rows-3 text-center">
         <div class="row-span-2">
-          <img src="../assets/k7.jpg" @click="$emit('setWindow', 3)" class="h-24 w-28 transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-125 hover:duration-300" />
+          <img src="../assets/k7.jpg" @click="$emit('setWindow', 3)" class="h-20 w-24 transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-125 hover:duration-300" />
         </div>
         <div class="row-span-1">
           <a class="hover:text-blue-300" @click="$emit('setWindow', 3)">???</a>
@@ -43,7 +43,15 @@
       </div>
       <div class="grid grid-rows-3 text-center">
         <div class="row-span-2">
-          <img src="../assets/k5.jpg" @click="$emit('modal')" class="h-24 w-28 transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-125 hover:duration-300" />
+          <img src="../assets/k8.jpg" @click="$emit('setWindow', 3)" class="h-20 w-24 transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-125 hover:duration-300" />
+        </div>
+        <div class="row-span-1">
+          <a class="hover:text-blue-300" @click="$emit('setWindow', 3)">???</a>
+        </div>
+      </div>
+      <div class="grid grid-rows-3 text-center">
+        <div class="row-span-2">
+          <img src="../assets/k5.jpg" @click="$emit('modal')" class="h-20 w-24 transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-125 hover:duration-300" />
         </div>
         <div class="row-span-1">
           <a v-if="uid === ''" @click="$emit('modal')" class="hover:text-blue-300">Sign In</a>
