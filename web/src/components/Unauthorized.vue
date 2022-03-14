294773265c662ca57b07c5a2e08fe9f427af76ff
rconjoe <joe@rconjoe.com>
2022-03-14 16:48:27 -0700

    init unauthorized component

---
+++ web/src/components/Unauthorized.vue
@@ -0,0 +1,12 @@
+<script setup>
+
+</script>
+
+<template>
+
+  <div class="flex flex-col-auto items-center justify-evenly bg-pink-300 border-2 shadow-xl border-x-pink-600 border-y-pink-800 rounded-xl p-12 mx-12 mb-8">
+    I didn't find anything here! Maybe you need to sign in?
+    <img src="../assets/kirbyfishing.jpg" class="max-w-xs xl:max-w-sm"/>
+  </div>
+
+</template>
