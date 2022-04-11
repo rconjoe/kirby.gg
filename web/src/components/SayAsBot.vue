1ae16389a3da79bd2163d1235cdd3471f260bcdc
rconjoe <joe@rconjoe.com>
2022-04-10 17:33:23 -0700

    right now this is supposed to write the input text to a db entry, which would be watched by a serverless firestore trigger function. but instead we are going to write a simple api endpoint to do it

--- web/src/components/SayAsBot.vue
+++ web/src/components/SayAsBot.vue
@@ -1,10 +1,29 @@
 <script setup>
+import { ref } from 'vue'
+import { firestore } from '../firebase.js'
+import { doc, setDoc } from 'firebase/firestore'
+
+const textToSay = ref("")
+
+// this function writes the text to a document in firestore, triggering a serverless function that does the bot.Say()
+// in other words, this is a whole other bot instance lol
+function sayAsKassandra() {
+  setDoc(doc(firestore, 'say-as-bot', 'text-to-say'), {
+    value: textToSay.value
+  })
+    .then(() => {
+      textToSay.value = ""
+    })
+  .catch((err) => {
+    alert('There was an error sending your message')
+  })
+}
 
 </script>
 
 <template>
   <div class="flex flex-row m-1 col-span-4 justify-center lg:ml-4">
-    <input type="text" placeholder="Say something (as Kassandra)!" class="bg-gray-200 border-2 w-full p-2 text-lg rounded-lg xl:row-span-2 shadow-lg" />
+    <input type="text" v-model="textToSay" placeholder="Say something (as Kassandra)!" class="bg-gray-200 border-2 w-full p-2 text-lg rounded-lg xl:row-span-2 shadow-lg" />
     <button @click="sayAsKassandra" class="px-4 ml-4 bg-pink-800 rounded-lg text-white transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 hover:duration-300 hover:shadow-xl">Send</button>
   </div>
 </template>
