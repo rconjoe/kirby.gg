7801609dc9a762474e4754a8538248541fd0e9db
rconjoe <joe@rconjoe.com>
2022-04-10 17:30:49 -0700

    init getSchedule function

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,15 @@
 const functions = require("firebase-functions");
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+exports.getSchedule = functions.https.onCall(async (data, context) => {
+  const schedule = []
+  const sched = await axios.get('https://api.twitch.tv/helix/schedule?broadcaster_id=18846157', {
+    headers: {
+      'Authorization': `Bearer ${process.env.TWITCH_TOKEN}`,
+      'Client-Id': `${process.env.TWITCH_ID}`
+    }
+  })
+  for (let i = 0; i < 5; i++) {
+    schedule.value.push(sched.data.data.segments[i])
+  }
+  return schedule;
+});
